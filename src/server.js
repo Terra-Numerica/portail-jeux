2d99d7d042f7f7a4b86ab1f920eb3a5826657e2e
KushiieDev <riviere.lisa1875@gmail.com>
2023-06-09 15:06:26 +0200

    feat: Add render view if error 404 occured

--- src/server.js
+++ src/server.js
@@ -12,4 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(resolve('./src/public')));
 app.use(express.json());
 
+app.get('*', (_, res) => res.status(404).render('errors/404'));
+
 app.listen(process.env.PORT, () => console.log(`[Portail Jeux] Listen on port ${process.env.PORT}`));
