882ac08dfac8c7b72d0b2be7354c43c93cc11ec4
Megitsune <riviere.lisa1875@gmail.com>
2023-06-22 11:12:39 +0200

    feat: Add getGame function

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,3 +3,7 @@ const APIBaseURL = process.env.API_BASE_URL;
 export async function getGames(params = "") {
     return await fetch(params === "" ? APIBaseURL : APIBaseURL + params).then((res) => res.json());
 };
+
+export async function getGame(jeuId) {
+    return (await getJeux()).find((jeu) => jeu.id === jeuId);
+};
