e22fc8167556d3efc15a817fcedb33b0f44079f9
Megitsune <riviere.lisa1875@gmail.com>
2023-07-24 09:56:41 +0200

    feat: Update getGames to adapt unknown illustration

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,7 +1,20 @@
 const APIBaseURL = process.env.API_BASE_URL;
 
 export async function getGames(params = "") {
-    return await fetch(params === "" ? APIBaseURL : APIBaseURL + params).then((res) => res.json());
+
+    const jeux = await fetch(params === "" ? APIBaseURL : APIBaseURL + params)
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
+
+    return jeux.map((jeu) => {
+
+        const illustration = jeu.illustration ? jeu.illustration : '/images/unknown_illustration.png';
+
+        return {
+            ...jeu,
+            illustration
+        }
+    });
 };
 
 export async function getGame(jeuId) {
